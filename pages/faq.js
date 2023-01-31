8ebde469f68e66fdc9cfdeda094e65615641b176
Ariane Brandao <ariplaymad@gmail.com>
2023-01-30 22:55:13 -0300

    feat: finaliza componentes

--- pages/faq.js
+++ pages/faq.js
@@ -1,7 +1,11 @@
-import Link from '../src/components/Link';
-import Head from 'next/head';
+// faq.js
+import FAQScreen from '../src/screens/FAQScreen';
 
-export async function getStaticProps(context) {
+export default FAQScreen; 
+
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
         .then((respostaDoServidor) => {
@@ -13,30 +17,8 @@ export async function getStaticProps(context) {
 
     return {
       props: {
-        faq
-      }, // will be passed to the page component as props
-    }
-  }
-
-export default function FAQ({faq}) {
-    return (
-        <div>
-            <Head>
-                <title>FAQ - Alura Cases Campanha</title>
-            </Head>
-            <h1>FAQ page</h1>
-            <Link href="/">ir para home</Link>
-
-            <ul>
-                {faq.map(({answer, question}) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
